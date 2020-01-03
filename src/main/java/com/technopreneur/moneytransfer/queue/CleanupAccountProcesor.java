48e54016870c349237f7fe94804aeedc969fff10
Milind-Technopreneur <milind.technopreneur@gmail.com>
2020-01-03 07:49:50 +0100

    commit

---
+++ src/main/java/com/technopreneur/moneytransfer/queue/CleanupAccountProcesor.java
@@ -0,0 +1,7 @@
+package com.technopreneur.moneytransfer.queue;
+
+public interface CleanupAccountProcesor {
+	
+	public void cleanup();
+
+}
---
+++ src/main/java/com/technopreneur/moneytransfer/queue/SimpleCleanupAccountProcessor.java
@@ -0,0 +1,17 @@
+package com.technopreneur.moneytransfer.queue;
+
+public class SimpleCleanupAccountProcessor implements CleanupAccountProcesor, Runnable {
+
+	@Override
+	public void cleanup() {
+		// TODO 
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Clean up AccountProcessor occasionally..
+		
+	}
+
+}
